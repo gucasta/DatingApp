04f68b83ea1bb3741ab8ffbf938387daba3a618c
Memo Martell <gucasta@me.com>
2024-01-03 13:34:17 -0600

    End of section 5

--- API/Program.cs
+++ API/Program.cs
@@ -15,7 +15,8 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod()
-    .WithOrigins("https://localhost:4200"));
+    .WithOrigins("http://localhost:4200"));
+    //.WithOrigins("https://localhost:4200")); //HTTPS
 
 app.UseAuthentication();
 app.UseAuthorization();
